ad9add6497f728ff7e5ceb3cadd80384ea5bdd04
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2023-09-14 23:59:38 -0300

    mudanca de rota

--- front-ListagemDeClientes/src/app/app-routing.module.ts
+++ front-ListagemDeClientes/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientesFormComponent } from './views/clientes-form/clientes-form.component';
+import { ClientesComponent } from './views/clientes/clientes.component';
 
 const routes: Routes = [{
-  path: '', component: ClientesFormComponent
+  path: '', component: ClientesComponent
 }];
 
 @NgModule({
